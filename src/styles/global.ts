51490b9534fa08d45091d45c40de454ad30425c2
Lucas Menezes <luckmenez@gmail.com>
2021-11-24 01:21:14 -0300

    [enhancement|component:NewTransactionModal]:Created transaction modal forms and first styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -28,15 +28,6 @@ export const GlobalStyle = createGlobalStyle`
     @media (max-width: 720px){
       font-size: 87.5%;
     }
-
-    button {
-      cursor: pointer;
-    }
-
-    [disable] {
-      opacity: 0.6;
-      cursor: not-allowed;
-    }
   }
 
   body {
@@ -51,5 +42,37 @@ export const GlobalStyle = createGlobalStyle`
 
   h1, h2, h3, h4, h5, h6, strong{
     font-weight: 600;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disable] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+  
+  .react-modal-overlay{
+    background: rgba(0, 0, 0, 0.5);
+    
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  
+  .react-modal-content{
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem;
   }
 `
