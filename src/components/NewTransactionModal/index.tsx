051a54e435d9dc42870282128a90ea381f72033b
Lucas Menezes <luckmenez@gmail.com>
2021-11-27 15:10:22 -0300

    [enhancement|RadioBox dynamic stylization]:implemented button activity stylization

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 
-import { Container, TransactionTypeContainer } from "./styles";
+import { 
+  Container, 
+  TransactionTypeContainer,
+  RadioBox, 
+} from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,6 +16,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionModalProps){
+  const [type, setType] = useState('deposit');
+  
   return(
     <Modal
       isOpen={isOpen}
@@ -39,18 +46,25 @@ export function NewTransactionModal({ isOpen, onRequestClose }:NewTransactionMod
         />
 
         <TransactionTypeContainer>
-          <button
-            type="button"            
+          <RadioBox
+            type="button"
+            onClick={() => { setType('deposit'); }}
+            isActive={type === 'deposit'}
+            activeColor="green"                        
           >
             <img src={incomeImg} alt="Entradas"/>
             <span>Entrada</span>
-          </button>
-          <button
-            type="button"            
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => { setType('withdraw'); }}
+            isActive={type === 'withdraw'}
+            activeColor="red"            
           >
             <img src={outcomeImg} alt="Saída"/>
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
 
         <input
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
   h2 {
@@ -49,5 +50,50 @@ export const Container = styled.form`
 `;
 
 export const TransactionTypeContainer = styled.div`
-
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap:0.5rem;
 `;
+
+interface RadioBoxProps  {
+  isActive: boolean;
+  activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33cc95',
+  red: '#E52E4D',
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 4rem;
+  border:1px solid #d7d7d7;
+    
+  background: ${ ({isActive, activeColor }) => isActive 
+    ? transparentize(0.9, colors[activeColor]) 
+    : 'transparent'
+  };
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.2, '#d7d7d7')};
+  } 
+
+  img {
+    height: 20px;
+    width: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
+    
+`
